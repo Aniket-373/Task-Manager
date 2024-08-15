0804351583d1a4a4d07d2d18e1a652811bf6e6f0
Aniket Bhoir <aniketbhoir373@gmail.com>
2024-08-15 14:37:44 +0530

    first commit

---
+++ routes/auth.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+
+// Middleware function to authenticate a JWT token
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (token == null) {
+        return res.status(401).json({ message: 'Authentication token required' });
+    }
+
+    jwt.verify(token, "ab1TM", (err, user) => {
+        if (err) {
+            return res.status(403).json({ message: 'Invalid or expired token' });
+        }
+        req.user = user;
+        next();
+    });
+}
+
+module.exports = {authenticateToken};
